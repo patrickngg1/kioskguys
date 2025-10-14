348a07a843972ec593264d81fc0db9154a634ed5
Prakash Sapkota <optimistic.prakash@gmail.com>
2025-10-14 04:30:23 -0500

    Set Up Communication (CORS and API Proxy)

--- kiosks/kiosks/settings.py
+++ kiosks/kiosks/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'main.apps.MainConfig',
+    'corsheaders'
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    # Allows your React dev server to communicate
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173", # Standard Vite port
+    "http://127.0.0.1:5173",
 ]
 
 ROOT_URLCONF = 'kiosks.urls'
