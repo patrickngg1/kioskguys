16eac4ddee0c35de8011fb9715e11b8a43a277b4
thiennguyen <tpn3461@mavs.uta.edu>
2026-02-02 23:34:05 -0600

    small fixx

--- smartKiosk/backend/kiosks/wsgi.py
+++ smartKiosk/backend/kiosks/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartKiosk.kiosks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kiosks.settings')
 
 application = get_wsgi_application()
