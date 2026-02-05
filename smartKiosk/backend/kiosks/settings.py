5e4325759751f64e4caee78a6e08fb2681acf4fd
Thien Phuoc Nguyen <tpn3461@mavs.uta.edu>
2026-02-04 23:49:28 -0600

    Add STATICFILES_DIRS for static file configuration

--- smartKiosk/backend/kiosks/settings.py
+++ smartKiosk/backend/kiosks/settings.py
@@ -75,6 +75,7 @@ MIDDLEWARE = [
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # ---------------------------------------------------------
