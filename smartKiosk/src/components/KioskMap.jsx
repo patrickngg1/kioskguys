2d2767f641a24b35da646e2cb3960c7ea4c3392e
Earl Sanchez <exs2756@mavs.uta.edu>
2026-03-07 17:27:49 -0600

    fixed ersa maps

--- smartKiosk/src/components/KioskMap.jsx
+++ smartKiosk/src/components/KioskMap.jsx
@@ -2,7 +2,7 @@ export default function KioskMap() {
   return (
     <iframe
       id='map-frame'
-      src='https://app.mappedin.com/map/692e274ca10fac000b4ee632?embedded=true'
+      src='https://app.mappedin.com/map/69a87115d465f9000b6f158d'
       title='Mappedin Map'
       allow="clipboard-write 'self' https://app.mappedin.com; web-share 'self' https://app.mappedin.com"
       sandbox='allow-scripts allow-same-origin allow-popups'
